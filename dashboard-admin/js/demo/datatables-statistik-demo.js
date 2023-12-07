b87af1078d27a4247858b901c3a3da37fae4762e
Mahendrata Harpi <id.harpi@pm.me>
2023-12-07 17:42:48 +0700

    update add example table pekerjaan, status perkawinan, agama, jenis kelamin

--- dashboard-admin/js/demo/datatables-statistik-demo.js
+++ dashboard-admin/js/demo/datatables-statistik-demo.js
@@ -73,4 +73,104 @@ $(document).ready(function () {
     .buttons()
     .container()
     .appendTo("#dataTable-pendidikan-sedang-ditempuh_wrapper .col-md-6:eq(0)");
+
+  const tablePekerjaan = $("#dataTable-pekerjaan").DataTable({
+    lengthChange: false,
+    buttons: [
+      {
+        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
+        action: function (e, dt, node, config) {
+          dt.ajax.reload();
+        },
+      },
+      {
+        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
+        split: ["csv", "pdf", "excel"],
+      },
+      {
+        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
+        extend: "colvis",
+      },
+    ],
+  });
+
+  tablePekerjaan
+    .buttons()
+    .container()
+    .appendTo("#dataTable-pekerjaan_wrapper .col-md-6:eq(0)");
+
+  const tableStatusPerkawinan = $("#dataTable-status-perkawinan").DataTable({
+    lengthChange: false,
+    buttons: [
+      {
+        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
+        action: function (e, dt, node, config) {
+          dt.ajax.reload();
+        },
+      },
+      {
+        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
+        split: ["csv", "pdf", "excel"],
+      },
+      {
+        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
+        extend: "colvis",
+      },
+    ],
+  });
+
+  tableStatusPerkawinan
+    .buttons()
+    .container()
+    .appendTo("#dataTable-status-perkawinan_wrapper .col-md-6:eq(0)");
+
+  const tableAgama = $("#dataTable-agama").DataTable({
+    lengthChange: false,
+    buttons: [
+      {
+        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
+        action: function (e, dt, node, config) {
+          dt.ajax.reload();
+        },
+      },
+      {
+        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
+        split: ["csv", "pdf", "excel"],
+      },
+      {
+        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
+        extend: "colvis",
+      },
+    ],
+  });
+
+  tableAgama
+    .buttons()
+    .container()
+    .appendTo("#dataTable-agama_wrapper .col-md-6:eq(0)");
+
+  const tableJenisKelamin = $("#dataTable-jenis-kelamin").DataTable({
+    lengthChange: false,
+    buttons: [
+      {
+        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
+        action: function (e, dt, node, config) {
+          dt.ajax.reload();
+        },
+      },
+      {
+        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
+        split: ["csv", "pdf", "excel"],
+      },
+      {
+        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
+        extend: "colvis",
+      },
+    ],
+  });
+
+  tableJenisKelamin
+    .buttons()
+    .container()
+    .appendTo("#dataTable-jenis-kelamin_wrapper .col-md-6:eq(0)");
 });
