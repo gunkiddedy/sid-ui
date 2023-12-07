ac43ffb12bf56c3fe8f24ecbf267fc211002e3e1
Mahendrata Harpi <id.harpi@pm.me>
2023-12-07 18:56:46 +0700

    update: feat(table-statistik) warga negara, golongan darah, penyandang cacat, status penduduk

--- dashboard-admin/js/demo/datatables-statistik-demo.js
+++ dashboard-admin/js/demo/datatables-statistik-demo.js
@@ -1,45 +1,33 @@
 // Call the dataTables jQuery plugin
+
+const defaultButton = [
+  {
+    text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
+    action: function (e, dt, node, config) {
+      dt.ajax.reload();
+    },
+  },
+  {
+    text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
+    split: ["csv", "pdf", "excel"],
+  },
+  {
+    text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
+    extend: "colvis",
+  },
+];
+
 $(document).ready(function () {
   const table = $("#dataTable").DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   table.buttons().container().appendTo("#dataTable_wrapper .col-md-6:eq(0)");
 
   const tablePendidikanDalamKK = $("#dataTable-pendidikan-dalam-kk").DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   tablePendidikanDalamKK
@@ -51,22 +39,7 @@ $(document).ready(function () {
     "#dataTable-pendidikan-sedang-ditempuh"
   ).DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   tablePendidikanSedangDitempuh
@@ -76,22 +49,7 @@ $(document).ready(function () {
 
   const tablePekerjaan = $("#dataTable-pekerjaan").DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   tablePekerjaan
@@ -101,22 +59,7 @@ $(document).ready(function () {
 
   const tableStatusPerkawinan = $("#dataTable-status-perkawinan").DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   tableStatusPerkawinan
@@ -126,22 +69,7 @@ $(document).ready(function () {
 
   const tableAgama = $("#dataTable-agama").DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   tableAgama
@@ -151,26 +79,51 @@ $(document).ready(function () {
 
   const tableJenisKelamin = $("#dataTable-jenis-kelamin").DataTable({
     lengthChange: false,
-    buttons: [
-      {
-        text: `<i class="fa fa-trash" aria-hidden="true"></i></i>&nbsp;Hapus Data`,
-        action: function (e, dt, node, config) {
-          dt.ajax.reload();
-        },
-      },
-      {
-        text: `<i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak Data`,
-        split: ["csv", "pdf", "excel"],
-      },
-      {
-        text: `<i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Preferensi Kolom`,
-        extend: "colvis",
-      },
-    ],
+    buttons: defaultButton,
   });
 
   tableJenisKelamin
     .buttons()
     .container()
     .appendTo("#dataTable-jenis-kelamin_wrapper .col-md-6:eq(0)");
+
+  const tableWargaNegara = $("#dataTable-warga-negara").DataTable({
+    lengthChange: false,
+    buttons: defaultButton,
+  });
+
+  tableWargaNegara
+    .buttons()
+    .container()
+    .appendTo("#dataTable-warga-negara_wrapper .col-md-6:eq(0)");
+
+  const tableGolonganDarah = $("#dataTable-golongan-darah").DataTable({
+    lengthChange: false,
+    buttons: defaultButton,
+  });
+
+  tableGolonganDarah
+    .buttons()
+    .container()
+    .appendTo("#dataTable-golongan-darah_wrapper .col-md-6:eq(0)");
+
+  const tablePenyandangCacat = $("#dataTable-penyandang-cacat").DataTable({
+    lengthChange: false,
+    buttons: defaultButton,
+  });
+
+  tablePenyandangCacat
+    .buttons()
+    .container()
+    .appendTo("#dataTable-penyandang-cacat_wrapper .col-md-6:eq(0)");
+
+  const tableStatusPenduduk = $("#dataTable-status-penduduk").DataTable({
+    lengthChange: false,
+    buttons: defaultButton,
+  });
+
+  tableStatusPenduduk
+    .buttons()
+    .container()
+    .appendTo("#dataTable-status-penduduk_wrapper .col-md-6:eq(0)");
 });
